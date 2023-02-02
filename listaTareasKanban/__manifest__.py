88685293657b8f0ca7e43a90dfcd0c6615e7142c
Ismael Fuentes <ifuentes17730@iesjoanramis.org>
2023-02-02 16:22:52 +0000

    ListaTareas update

--- listaTareasKanban/__manifest__.py
+++ listaTareasKanban/__manifest__.py
@@ -7,10 +7,8 @@
     'category': 'Project',
     'version': '1.0',
     'application': True,
-
     #Importante! Se basa en toda la estructura de project de Odoo
     #https://www.odoo.com/documentation/15.0/es/applications/services/project.html
-
     'depends': ['project'],
     'data': [
         'views/view_tareas.xml',
