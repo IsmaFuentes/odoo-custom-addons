fef517979b61cd1e0beb14119f186d79c6309dc1
Ismael Fuentes <ifuentes17730@iesjoanramis.org>
2023-03-02 16:40:56 +0000

    new module "ciclos"

---
+++ ciclos/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Ciclos formativos", 
+    'summary': "Gestor de ciclos formativos",
+    'description': "Gestiona tus própios ciclos formativos, crea módulos, añade alumnos y profesores.",  
+    'application': True,
+    'author': "Ismael Fuentes Sintes",
+    'website': "http://iesjoanramis.org",
+    'category': 'Tools',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/hospital_medico.xml',
+        # 'views/hospital_paciente.xml',
+        # 'views/hospital_diagnostico.xml'
+    ],
+}
